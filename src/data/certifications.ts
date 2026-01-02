fbe58193ab6c21d47c22c3d4a7c1f2a18a8c0624
Suraj13SK <13.suraj.s.k@gmail.com>
2026-01-02 18:21:08 +0530

    New Prod Ready

--- src/data/certifications.ts
+++ src/data/certifications.ts
@@ -1,13 +1,9 @@
 export const certifications = {
   achievements: [
-    "2nd Prize – National Level Research Poster Making Competition",
-    "2nd Prize – Poster Title Competition (College Level)",
     "2nd Prize – Intra-Collegiate Project Competition (2022–23)"
   ],
 
   certifications: [
-    "Spoken Tutorial – C++ (IIT Bombay)",
-    "Introduction to Internet of Things – NPTEL (SWAYAM, IITs)",
     "Cyber Security & Ethical Hacking – Level I",
     "Database Management System",
     "MS-CIT (Maharashtra State Certificate in IT)"
